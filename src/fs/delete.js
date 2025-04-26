616b51a4db63ddd9208d3d8cd04473e6d2841c81
mattklinkham <mattklinkham@gmail.com>
2025-04-26 21:47:04 +0500

    feat(fs): implement basic file operations

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const FILE_NAME = 'fileToRemove.txt';
+const FILES_DIR_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, FILES_DIR_NAME, FILE_NAME);
+
+    try {
+        await fs.rm(filePath);
+    } catch(error) {
+        throw new Error(ERROR_MESSAGE);
+    }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const FILES_DIR_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = join(__dirname, FILES_DIR_NAME);
+
+    try {
+        const files = await fs.readdir(folderPath, { withFileTypes: true });
+        const fileNames = files
+            .filter((dirent) => dirent.isFile())
+            .map((dirent) => dirent.name);
+
+        console.log(fileNames);
+    } catch(error) {
+        throw new Error(ERROR_MESSAGE);
+    }
 };
 
 await list();
