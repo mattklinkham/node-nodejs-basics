ba92282a302b09072897406b6d3e923fdc26499e
mattklinkham <mattklinkham@gmail.com>
2025-04-27 18:14:44 +0500

    feat(streams): implement streams subtasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const FILES_DIR_NAME = 'files';
+const READ_FILE_NAME = 'fileToRead.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, FILES_DIR_NAME, READ_FILE_NAME);
+    const rs = createReadStream(filePath);
+
+    rs.pipe(process.stdout);
+    rs.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,48 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const FILES_DIR_NAME = 'files';
+const WRITE_FILE_NAME = 'fileToWrite.txt';
+const START_MESSAGE =
+    'To finish the input and save it to the file, type "CLOSE" and press Enter.\n' +
+    'Alternatively, you can press Ctrl+C to terminate the process.\n';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePathToWrite = join(__dirname, FILES_DIR_NAME, WRITE_FILE_NAME);
+
+    const ws = fs.createWriteStream(filePathToWrite);
+
+    process.stdin.pipe(ws);
+
+    process.stdout.write(START_MESSAGE);
+
+    process.stdin.on('data', (chunk) => {
+        const chunkStringified = chunk.toString().trim();
+
+        if (chunkStringified === 'CLOSE') {
+            console.log('Closing the stream...');
+            process.stdin.emit('end');
+        }
+    });
+
+    process.stdin.on('end', () => {
+        console.log('File has been written successfully!');
+        ws.end();
+    });
+
+    process.on('SIGINT', () => {
+        console.log('Process interrupted. File write was aborted.');
+        ws.end();
+        process.exit(0);
+    });
+
+    ws.on('error', (error) => {
+        console.error('Write stream error:', error);
+    });
 };
 
 await write();
