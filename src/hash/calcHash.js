6e63af9ece5e1a53e8ee99f4c41a617dcf85a0a9
mattklinkham <mattklinkham@gmail.com>
2025-04-28 22:18:41 +0500

    feat(hash): implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const FILES_DIR_NAME = 'files';
+const FILE_NAME = 'fileToCalculateHashFor.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, FILES_DIR_NAME, FILE_NAME);
+    const hash = createHash('sha256');
+    const rs = createReadStream(filePath);
+
+    rs.on('error', (error) => {
+        console.error('Read stream error:', error.message);
+    });
+
+    rs.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    rs.on('end', () => {
+        const hex = hash.digest('hex');
+        console.log(hex);
+    });
 };
 
 await calculateHash();
