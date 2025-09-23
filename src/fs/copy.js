b1f2308bca98727ba0ed31c6419a31c079e9a538
mattklinkham <mattklinkham@gmail.com>
2025-09-23 17:07:05 +0400

    feat(fs): implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs/promises';
+import { join } from 'node:path';
+import { getModulePaths } from '../helpers/index.js';
+import { FileSystemError } from '../helpers/error.js';
+
+const SOURCE_DIR_NAME = 'files';
+const TARGET_DIR_NAME = 'files_copy';
+
+const {__dirname} = getModulePaths(import.meta.url);
+const sourceDir = join(__dirname, SOURCE_DIR_NAME);
+const destDir = join(__dirname, TARGET_DIR_NAME);
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.cp(sourceDir, destDir, { force: false, errorOnExist: true, recursive: true });
+    } catch (error) {
+        throw new FileSystemError(sourceDir, error);
+    }
 };
 
 await copy();
