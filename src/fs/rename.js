8cb3ffac76404a30befc461fbbba3efc1a33def9
mattklinkham <mattklinkham@gmail.com>
2025-10-01 16:23:37 +0400

    feat(fs): implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,38 @@
+import * as fs from 'fs/promises';
+import { join } from 'node:path';
+import { getModulePaths, isFileExists } from '../helpers/index.js';
+import { FileSystemError } from '../helpers/error.js';
+
+const SOURCE_DIR_NAME = 'files';
+const WRONG_FILE_NAME = 'wrongFilename.txt';
+const NEW_FILE_NAME = 'properFilename.md';
+
+const {__dirname, __filename} = getModulePaths(import.meta.url);
+
+const sourceDir = join(__dirname, SOURCE_DIR_NAME);
+const wrongFilePath = join(sourceDir, WRONG_FILE_NAME);
+const newFilePath = join(sourceDir, NEW_FILE_NAME);
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const [newFileExists, wrongFileExists] = await Promise.all([
+            isFileExists(newFilePath),
+            isFileExists(wrongFilePath)
+        ]);
+
+        if (!wrongFileExists) {
+            throw new FileSystemError(wrongFilePath, new Error('File does not exist'));
+        }
+        if (newFileExists) {
+            throw new FileSystemError(newFilePath, new Error('File already exists'));
+        }
+
+        await fs.rename(wrongFilePath, newFilePath);
+
+        console.log(`File ${WRONG_FILE_NAME} was renamed to ${NEW_FILE_NAME}`);
+    } catch (error) {
+        throw new FileSystemError(__filename, error);
+    }
 };
 
 await rename();
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
+import * as fs from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 
@@ -10,3 +11,14 @@ export const getModulePaths = (importMetaUrl) => {
         __dirname
     };
 };
+
+export const isFileExists = (path) => fs.access(path)
+    .then(() => true)
+    .catch((error) => {
+        if (error.code === 'ENOENT') {
+            return false;
+        }
+
+        throw error;
+    }
+);
